3572056a396a78f56a620f7c610d360dc1075ca6
Corbett Bennett <corbettb@alleninstitute.org>
2020-07-06 20:19:50 -0700

    Create launch.bat

---
+++ launch.bat
@@ -0,0 +1,4 @@
+call C:\Users\svc_ccg\AppData\Local\Continuum\anaconda3\Scripts\activate.bat C:\Users\svc_ccg\AppData\Local\Continuum\anaconda3\
+call conda activate probeLocator
+call jupyter notebook
+cmd /k
