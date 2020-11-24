3c457c98f08cffc740e7fd62bc4cefdf45aecd5f
corbennett <corbennett@gmail.com>
2020-11-23 20:11:02 -0800

    features: ability to reload points is restored, points can be deleted with a right click, right click and drag is now required to clear points (instead of just right click), marker is a blue crosshairs, points can be clicked and dragged

--- pointAnnotator.py
+++ pointAnnotator.py
@@ -26,31 +26,71 @@ class pointAnnotator:
         self.ys = []
         self.annos = []
         self.labels = []
+        self.marker_str = 'b+'
+        self.last_click = None
         self.cid = im.figure.canvas.mpl_connect('button_press_event', self.onclick)
         self.cidrelease = im.figure.canvas.mpl_connect('button_release_event', self.onrelease)
         
         self.kid = im.figure.canvas.mpl_connect('key_press_event', self.keypress)
 
     def onclick(self, event):
-        if event.inaxes==self.respond_in_ax:
-            if event.button == 1:
-                print('x ' + str(event.xdata) + '\ty ' + str(event.ydata))
-                self.xs.append(event.xdata)
-                self.ys.append(event.ydata)
+        self.last_click = event
+        print('registered click')
+        #if event.inaxes==self.respond_in_ax:
+        #    if event.button == 1:
 
-                anno, = self.ax.plot(event.xdata, event.ydata, 'ro')
-                self.annos.append(anno)
-                lab = self.ax.text(event.xdata+0.2, event.ydata+0.2, str(len(self.annos)))
-                self.labels.append(lab)
-                self.im.figure.canvas.draw()  
-            else:
-                self.resetAnnotations()
+            #Do this on the release instead else:
+            #    idx = self.clicked_annotation(event)
+            #    if idx:
+            #        self.deleteAnnotation(idx)
+
+                    
     def onrelease(self, event):
         #check to see if this was a click and drag, in which case, don't draw point
         if event.inaxes==self.respond_in_ax:
-            distance = ((event.xdata-self.xs[-1])**2 + (event.ydata-self.ys[-1])**2)**0.5
-            if distance > 10:
-                self.deleteLastAnnotation()
+            distance = ((event.xdata-self.last_click.xdata)**2 + (event.ydata-self.last_click.ydata)**2)**0.5
+            if event.button == 1:
+                idx = self.clicked_annotation(self.last_click)
+                if not(idx is None):
+                    self.update_annotation(idx, event)
+                else:
+                    
+                    if distance < 10:
+                        print('x ' + str(event.xdata) + '\ty ' + str(event.ydata))
+                        self.xs.append(event.xdata)
+                        self.ys.append(event.ydata)
+
+                        anno, = self.ax.plot(event.xdata, event.ydata, self.marker_str)
+                        self.annos.append(anno)
+                        lab = self.ax.text(event.xdata+6, event.ydata-4, str(len(self.annos)))
+                        self.labels.append(lab)
+                        self.im.figure.canvas.draw()  
+            else:
+                if (distance < 10) and not(self.clicked_annotation(self.last_click) is None):
+                    self.deleteAnnotation(self.clicked_annotation(self.last_click))
+                if (distance > 10) and self.clicked_annotation(self.last_click)==None and self.clicked_annotation(event)==None:
+                    self.resetAnnotations()
+
+    def update_annotation(self, idx, event):
+        self.xs[idx] = event.xdata
+        self.ys[idx] = event.ydata
+        self.annos[idx].remove()
+        anno, = self.ax.plot(event.xdata, event.ydata, self.marker_str)
+        self.annos[idx] = anno                                                                          
+        self.labels[idx].remove()
+        lab = self.ax.text(event.xdata+6, event.ydata-4, str(idx+1))
+        self.labels[idx] = lab
+        self.im.figure.canvas.draw()                                                                                       
+                                                                                        
+                
+    def clicked_annotation(self, event):
+        for idx, (x,y) in enumerate(zip(self.xs, self.ys)):
+            distance = ((event.xdata-x)**2 + (event.ydata-y)**2)**0.5
+            if distance < 10:
+                return idx
+        return None
+
+                
     def keypress(self, event):
         if event.inaxes==self.respond_in_ax:
             self.deleteLastAnnotation()
@@ -65,14 +105,27 @@ class pointAnnotator:
         self.annos=[]
         self.labels=[]
     
+    def deleteAnnotation(self, idx):
+        self.xs.pop(idx)
+        xs = self.xs
+        self.ys.pop(idx)
+        ys = self.ys
+        self.resetAnnotations()
+        self.ys = ys
+        self.xs = xs
+        self.drawPoints()
+
+        
+        
     def deleteLastAnnotation(self):
-        self.xs = self.xs[:-1]
-        self.ys = self.ys[:-1]
-        self.annos[-1].remove()
-        self.labels[-1].remove()
-        self.annos = self.annos[:-1]
-        self.labels = self.labels[:-1]
-        self.im.figure.canvas.draw()
+        self.deleteAnnotation(-1)
+        #self.xs = self.xs[:-1]
+        #self.ys = self.ys[:-1]
+        #self.annos[-1].remove()
+        #self.labels[-1].remove()
+        #self.annos = self.annos[:-1]
+        #self.labels = self.labels[:-1]
+        #self.im.figure.canvas.draw()
     
     def loadPoints(self, points):
         try:
@@ -87,9 +140,9 @@ class pointAnnotator:
         
     def drawPoints(self):
         for i, (x,y) in enumerate(zip(self.xs, self.ys)):
-            anno, = self.ax.plot(x,y, 'ro')
+            anno, = self.ax.plot(x,y, self.marker_str)
             self.annos.append(anno)
-            lab = self.ax.text(x,y,str(i+1))
+            lab = self.ax.text(x+6,y-4,str(i+1))
             self.labels.append(lab)
             self.im.figure.canvas.draw()
             
@@ -138,9 +191,11 @@ def points_from_path(path, *args):
         if os.path.splitext(filename)[1]=='.npz':
             npz_count += 1
             match_count = 0
+            print('found npz '+filename)
             try:
                 npz_file = np.load(os.path.join(path, filename), allow_pickle=True)
-                stored_points = npz_file['imagePairCoordinates'][0]
+                stored_points = npz_file#npz_file['imagePairCoordinates'][0]
+                print(stored_points)
                 for array_name in stored_points:
                     match = True
                     for arg in args:
